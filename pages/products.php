5210b13e9791ed91347c1400028a9d31fdb9a9ca
Bryan Philip <bphilips.devops@gmail.com>
2026-02-20 08:08:31 +0300

    update of speciality image

--- pages/products.php
+++ pages/products.php
@@ -72,7 +72,7 @@
     <article class="tea-card" data-tags="specialty">
       <div class="media">
         <div class="ratio ratio-16x9">
-          <?php pictureTag('A_orthodox%20infusion%201','Specialty Tea'); ?>
+          <?php pictureTag('speciality_image','Specialty Tea'); ?>
         </div>
       </div>
       <div class="body">
